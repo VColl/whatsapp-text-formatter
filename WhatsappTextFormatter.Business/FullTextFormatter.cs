d54b579ef495a5b7f99d7c4409d29bb59785aaab
Victor Coll <victorcoll93@gmail.com>
2020-03-20 19:19:04 -0300

    Testes com múltiplos tipos isolados passando

--- WhatsappTextFormatter.Business/FullTextFormatter.cs
+++ WhatsappTextFormatter.Business/FullTextFormatter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace WhatsappTextFormatter.Business
 {
@@ -41,10 +42,35 @@ namespace WhatsappTextFormatter.Business
                 text = text.Remove(range.Item2 + 1, 1);
             }
 
+            var fixedItalics = new List<Tuple<int, int>>();
+            foreach (var italicRange in info.Italics)
+            {
+                var count1 = info.StrikeThroughs.Count(r => r.Item1 < italicRange.Item1) + info.StrikeThroughs.Count(r => r.Item2 < italicRange.Item1);
+                var count2 = info.StrikeThroughs.Count(r => r.Item1 < italicRange.Item2) + info.StrikeThroughs.Count(r => r.Item2 < italicRange.Item2);
+
+                fixedItalics.Add(Tuple.Create(italicRange.Item1 - count1, italicRange.Item2 - count2));
+            }
+
+            var fixedBolds = new List<Tuple<int, int>>();
+            foreach (var boldRange in info.Bolds)
+            {
+                var count1 = info.Italics.Count(r => r.Item1 < boldRange.Item1) + info.Italics.Count(r => r.Item2 < boldRange.Item1)
+                    + info.StrikeThroughs.Count(r => r.Item1 < boldRange.Item1) + info.StrikeThroughs.Count(r => r.Item2 < boldRange.Item1);
+
+                var count2 = info.Italics.Count(r => r.Item1 < boldRange.Item2) + info.Italics.Count(r => r.Item2 < boldRange.Item2)
+                    + info.StrikeThroughs.Count(r => r.Item1 < boldRange.Item2) + info.StrikeThroughs.Count(r => r.Item2 < boldRange.Item2);
+
+                fixedBolds.Add(Tuple.Create(boldRange.Item1 - count1, boldRange.Item2 - count2));
+            }
+
             info.Text = text;
+            info.Bolds = fixedBolds;
+            info.Italics = fixedItalics;
+
             return info;
         }
 
-        
+        private IEnumerable<int> GetIndexes(IEnumerable<Tuple<int, int>> indexRanges) =>
+            indexRanges.SelectMany(range => Enumerable.Range(range.Item1, range.Item2));
     }
 }
